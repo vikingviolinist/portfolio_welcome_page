ababb88fed7f93763e36d3dc3147e801406b6d18
vikingviolinist <misopetrik@gmail.com>
2018-05-06 10:58:35 +0200

    Change language of anchor tag in navbar instead of li element

--- main.js
+++ main.js
@@ -21,32 +21,32 @@ $(document).ready(function () {
   $('#button-norwegian').on('click', () => {
     $('h1').text('Velkommen til min webside!');
     $('h3').text('Jeg heter Michal og jeg liker å bygge ting.');
-    $('.navbar-link:nth(0)').text('om meg');
-    $('.navbar-link:nth(2)').text('utdanning');
+    $('.navbar-link a:nth(0)').text('om meg');
+    $('.navbar-link a:nth(2)').text('utdanning');
     $('button').text('litt mer om meg');
   });
 
   $('#button-english').on('click', () => {
     $('h1').text('Welcome to my webpage!');
     $('h3').text('My name is Michal and I like to build stuff.');
-    $('.navbar-link:nth(0)').text('about me');
-    $('.navbar-link:nth(2)').text('education');
+    $('.navbar-link a:nth(0)').text('about me');
+    $('.navbar-link a:nth(2)').text('education');
     $('button').text('more about me');
   });
 
   $('#button-german').on('click', () => {
     $('h1').text('Willkommen auf meiner Webseite!');
     $('h3').text('Ich heiße Michal und mag Dinge zu bauen.');
-    $('.navbar-link:nth(0)').text('über mich');
-    $('.navbar-link:nth(2)').text('ausbildung');
+    $('.navbar-link a:nth(0)').text('über mich');
+    $('.navbar-link a:nth(2)').text('ausbildung');
     $('button').text('mehr über mich');
   });
 
   $('#button-slovak').on('click', () => {
     $('h1').text('Vitajte na mojich webových stránkach!');
     $('h3').hide();
-    $('.navbar-link:nth(0)').text('o mne');
-    $('.navbar-link:nth(2)').text('vzdelanie');
+    $('.navbar-link a:nth(0)').text('o mne');
+    $('.navbar-link a:nth(2)').text('vzdelanie');
     $('button').text('viac info');
   });
 
